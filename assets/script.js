d982f2087b496541acba4b6309b2c3f16a4e84dc
shinobiKoda <sirp2804@gmail.com>
2024-10-06 15:24:50 +0100

    added mobile hero and shorten link section

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,24 @@
 const hamburger = document.querySelector(".hamburger");
 const mobile_nav_links = document.querySelector(".mobile-nav-links");
+const nav_link = document.querySelectorAll(".nav-link");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("rotate");
   mobile_nav_links.classList.toggle("display");
 });
+
+nav_link.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (mobile_nav_links.classList.contains("display")) {
+      mobile_nav_links.classList.remove("display");
+      hamburger.classList.remove("rotate");
+    }
+  });
+});
+
+// document.addEventListener("click", () => {
+//   if (mobile_nav_links.classList.contains("display")) {
+//     mobile_nav_links.classList.remove("display");
+//     hamburger.classList.remove("rotate");
+//   }
+// });
